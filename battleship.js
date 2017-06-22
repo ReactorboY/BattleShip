4502bba0a8fbbd26e579abb76a534b339981c902
Muhammad Hussain <samsteve124@gmail.com>
2017-06-22 12:04:17 +0530

    chnaged

--- battleship.js
+++ battleship.js
@@ -1,6 +1,49 @@
  // MVC is the most important thing to learn while
  // designing single page applications as well as games
 
+//  model object
+//  this initiize everytnig for game
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [
+    { locations: ["06","16","26"], hits: ["","",""] },
+    { locations: ["24","34","44"], hits: ["","",""] },
+    { locations: ["10","11","12"], hits: ["","",""] }
+  ],
+
+  fire: function (guess) {
+    for(var i = 0;i < this.numShips; i++){
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      if(index >= 0){
+        ships.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT")
+        if(this.isSunk(ship)){
+          view.displayMessage("You sank my ship!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed")
+    return false;
+  },
+  isSunk: function (ship) {
+    for(var i = 0; i < this.shipLength; i++){
+      if(ship.hits[i] !== "hit"){
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
 var view = {
   displayMessage: function (msg) {
     var messageArea = document.getElementById("messageArea");
@@ -15,3 +58,5 @@ var view = {
     cell.setAttribute("class", "miss");
   }
 };
+
+model.fire("53");
